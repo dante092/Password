4e3a9de43fad8b1801228b296a2ce7a11d63f475
dante092 <gregborrelly@gmail.com>
2016-02-17 00:27:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import password, getpass
 character_list = []
 digit_list = []
+alpha_list=[]
 
 print('\n')
 print(' PASSWORD STRENGTH TEST'.center(40, '='))
 
 testpassword = getpass.getpass('\nPASSWORD: ')
+print('PASWORD RESULTS'.center(40,'='))
 
 # Uses Regexes to examine password.
 digit_list = password.digitRegex(testpassword, digit_list)
 character_list = password.characterRegex(testpassword,character_list)
-
-print('--- PASSWORD HAS ' + str(len(character_list)) + ' CHARACTERS')
-print('--- PASSWORD HAS ' + str(len(digit_list)) + ' DIGITS\n')
-print('PASWORD RESULTS'.center(40,'='))
+alpha_list= password.AlphaRegex(testpassword, alpha_list)
 
 # reads the data from the Regexes and determines strength of Password.
-password.test(character_list,digit_list)
+password.test(character_list,digit_list, alpha_list)
+
+print('     -- PASSWORD HAS ' + str(len(character_list)) + ' CHARACTERS')
+print('     -- PASSWORD HAS ' + str(len(digit_list)) + ' DIGITS')
+print('     -- PASSWORD HAS ' + str(len(alpha_list))+ ' UPPERCASE LETTERS')
+
+
+
+
+
 
 
